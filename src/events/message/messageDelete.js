e2eac3934a5a1bf8204c6f8978efea42446b4722
Rizky Pratama <104710750+Aruh1@users.noreply.github.com>
2023-08-10 08:45:33 +0000

    addd snipe edited

--- src/events/message/messageDelete.js
+++ src/events/message/messageDelete.js
@@ -1,6 +1,4 @@
-const db = require("../../database/mongoose.js");
-const { deletedChannelId } = requrire("../../../config.js")
-const { MessageEmbed } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 const { getSettings } = require("@schemas/Guild");
 
 /**
@@ -8,63 +6,44 @@ const { getSettings } = require("@schemas/Guild");
  * @param {import('discord.js').Message|import('discord.js').PartialMessage} message
  */
 module.exports = async (client, message) => {
-    if (message.partial || message.author.bot || !message.guild) return;
+    if (message.partial) return;
+    if (message.author.bot || !message.guild) return;
 
     const settings = await getSettings(message.guild);
     if (!settings.automod.anti_ghostping || !settings.modlog_channel) return;
-
     const { members, roles, everyone } = message.mentions;
 
-    const deletedMessageEmbed = new MessageEmbed()
-        .setColor(0xff0000)
-        .setTitle("Message Delete")
-        .setAuthor(message.author.username, message.author.displayAvatarURL())
-        .addField("Content", `> ${message.content}`)
-        .addField("Channel", `<#${message.channel.id}>`)
-        .addFields(
-            { name: "Username", value: `<@${message.author.id}>`, inline: true },
-            { name: "User ID", value: message.author.id, inline: true },
-            { name: "Message ID", value: message.id, inline: true }
-        )
-        .setTimestamp()
-        .setFooter(client.user.username);
-
-    message.attachments.forEach(attachment => {
-        deletedMessageEmbed.addField("Attachment", attachment.url);
-    });
-
-    const deletedChannelId = settings.modlog_channel; // Define deletedChannelId
-
-    try {
-        const channel = await client.channels.fetch(deletedChannelId);
-        channel.send({
-            content: `DELETE: \`${message.author.username}\` (${message.author.id})`,
-            embeds: [deletedMessageEmbed]
-        });
-    } catch (error) {
-        console.error("Error sending deleted message log:", error);
-    }
-
+    // Check message if it contains mentions
     if (members.size > 0 || roles.size > 0 || everyone) {
         const logChannel = message.guild.channels.cache.get(settings.modlog_channel);
-        if (logChannel) {
-            const ghostPingEmbed = new MessageEmbed()
-                .setAuthor("Ghost ping detected")
-                .setDescription(
-                    `**Message:**\n${message.content}\n\n` +
-                    `**Author:** ${message.author.tag} \`${message.author.id}\`\n` +
-                    `**Channel:** ${message.channel}`
-                )
-                .addField("Members", members.size.toString(), true)
-                .addField("Roles", roles.size.toString(), true)
-                .addField("Everyone?", everyone ? "Yes" : "No", true)
-                .setFooter(`Sent at: ${message.createdAt}`);
+        if (!logChannel) return;
 
-            try {
-                logChannel.send({ embeds: [ghostPingEmbed] });
-            } catch (error) {
-                console.error("Error sending ghost ping log:", error);
-            }
-        }
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: "Ghost ping detected" })
+            .setDescription(
+                `**Message:**\n${message.content}\n\n` +
+                    `**Author:** ${message.author.tag} \`${message.author.id}\`\n` +
+                    `**Channel:** ${message.channel.toString()}`
+            )
+            .addFields(
+                {
+                    name: "Members",
+                    value: members.size.toString(),
+                    inline: true
+                },
+                {
+                    name: "Roles",
+                    value: roles.size.toString(),
+                    inline: true
+                },
+                {
+                    name: "Everyone?",
+                    value: everyone ? "Yes" : "No",
+                    inline: true
+                }
+            )
+            .setFooter({ text: `Sent at: ${message.createdAt}` });
+
+        logChannel.safeSend({ embeds: [embed] });
     }
 };
