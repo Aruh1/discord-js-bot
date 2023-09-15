d992d2a7da74396d2d8ff3f7c8fba6a6c10bbd80
XCR90 <90918605+XCR90@users.noreply.github.com>
2023-09-15 10:32:37 +0000

    update changelog to speedtest-net

--- src/commands/admin/speedtest.js
+++ src/commands/admin/speedtest.js
@@ -1,5 +1,17 @@
-const { MessageEmbed } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 const speedTest = require("speedtest-net");
+
+function createWaitingEmbed() {
+    return new EmbedBuilder()
+        .setTitle("Speedtest")
+        .setColor(0xffff00)
+        .setDescription("The Speedtest is running, please wait a bit.")
+        .setThumbnail(
+            "https://store-images.s-microsoft.com/image/apps.52586.13510798887693184.740d7baf-50aa-4e26-adec-ae739ac12068.c9ef9495-f245-4367-872b-c5cc7b48841d"
+        )
+        .setImage("https://b.cdnst.net/images/share-logo.png")
+        .setFooter({ text: "This takes approximately 30 seconds." });
+}
 
 module.exports = {
     name: "speedtest",
@@ -13,49 +25,54 @@ module.exports = {
         ephemeral: false,
         options: []
     },
-    async messageRun(_client, interaction) {
-        await this.interactionRun(_client, interaction);
-    },
-    async interactionRun(_client, interaction) {
-        const waitingEmbed = new MessageEmbed()
-            .setTitle("Speedtest")
-            .setColor(0xffff00)
-            .setDescription("The Speedtest is running, please wait a bit.")
-            .setThumbnail(
-                "https://store-images.s-microsoft.com/image/apps.52586.13510798887693184.740d7baf-50aa-4e26-adec-ae739ac12068.c9ef9495-f245-4367-872b-c5cc7b48841d"
-            )
-            .setImage("https://b.cdnst.net/images/share-logo.png")
-            .setFooter("This takes approximately 30 seconds.");
-
-        await interaction.reply({
+    async messageRun(message, args, data) {
+        const waitingEmbed = createWaitingEmbed();
+        const replyMessage = await message.safeReply({
             embeds: [waitingEmbed]
         });
 
-        try {
-            const speed = await speedTest({ acceptLicense: true });
-            const finishEmbed = new MessageEmbed()
-                .setTitle(`${_client.user.username} Speedtest`)
-                .setColor(0xffff00)
-                .setDescription(
-                    `The Speedtest is done, here is the result:\nLocal: ${speed.isp} \nServer: ${speed.server.name} | ${speed.server.location} | ID: ${speed.server.id}\nHost: ${speed.server.host}\nPacket-Loss: ${speed.packetLoss}\nIP: ${speed.server.ip}`
-                )
-                .setFooter("The Speedtest used is speedtest.net.")
-                .setThumbnail(
-                    "https://store-images.s-microsoft.com/image/apps.52586.13510798887693184.740d7baf-50aa-4e26-adec-ae739ac12068.c9ef9495-f245-4367-872b-c5cc7b48841d"
-                )
-                .addFields({ name: "Result", value: speed.result.url })
-                .setImage(`${speed.result.url}.png`);
+        runSpeedTestAndRespond(replyMessage);
+    },
 
-            interaction.editReply({ embeds: [finishEmbed] });
-        } catch (error) {
-            console.error("Speedtest error:", error);
-            const errorEmbed = new MessageEmbed()
-                .setTitle("Speedtest")
-                .setColor(0xff0000)
-                .setDescription("An error occurred while running the Speedtest. Please try again later.")
-                .setFooter("Error occurred during the Speedtest.");
+    async interactionRun(interaction, data) {
+        const waitingEmbed = createWaitingEmbed();
+        await interaction.editReply({
+            embeds: [waitingEmbed]
+        });
 
-            interaction.editReply({ embeds: [errorEmbed] });
-        }
+        runSpeedTestAndRespond(interaction);
     }
 };
+
+async function runSpeedTestAndRespond(target, data) {
+    try {
+        const speed = await speedTest({ acceptLicense: true });
+        const finishEmbed = new EmbedBuilder()
+            .setTitle(`${target.user.client.user.username} Speedtest`)
+            .setColor(0xffff00)
+            .setDescription(
+                `The Speedtest is done, here is the result:\nLocal: ${speed.isp}\nServer: ${speed.server.name} | ${speed.server.location} | ID: ${speed.server.id}\nHost: ${speed.server.host}\nPacket-Loss: ${speed.packetLoss}\nIP: ${speed.server.ip}`
+            )
+            .setFooter({ text: "The Speedtest used is speedtest.net." })
+            .setThumbnail(
+                "https://store-images.s-microsoft.com/image/apps.52586.13510798887693184.740d7baf-50aa-4e26-adec-ae739ac12068.c9ef9495-f245-4367-872b-c5cc7b48841d"
+            )
+            .addFields({ name: "Result", value: speed.result.url })
+            .setImage(`${speed.result.url}.png`);
+
+        await target.editReply({
+            embeds: [finishEmbed]
+        });
+    } catch (error) {
+        console.error("Speedtest error:", error);
+        const errorEmbed = new EmbedBuilder()
+            .setTitle("Speedtest")
+            .setColor(0xff0000)
+            .setDescription("An error occurred while running the Speedtest. Please try again later.")
+            .setFooter({ text: "Error occurred during the Speedtest." });
+
+        await target.editReply({
+            embeds: [errorEmbed]
+        });
+    }
+}
